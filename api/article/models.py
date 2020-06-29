9be90de0d24b0211cf29f0903afebc2536f73c9e
田端佑也 <tabayu5@gmail.com>
2020-06-29 23:09:26 +0900

    migrate

--- api/article/models.py
+++ api/article/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.utils import timezone
 
 class Category(models.Model):
-    name = models.Charfield('category_name', max_length=100)
-    color = models.Charfield('color',max_length=7, default='#000000')
+    name = models.CharField('category_name', max_length=100)
+    color = models.CharField('color',max_length=7, default='#000000')
 
     def __str__(self):
         return self.name
 
 class Post(models.Model):
-    title = models.Charfield('title', max_length=40)
+    title = models.CharField('title', max_length=40)
     thumbnail = models.ImageField('thumbnail', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Category') #Categoryが親
     lead_text = models.TextField('lead_text')
@@ -20,4 +20,4 @@ class Post(models.Model):
         ordering = ('-created_at',) #オブジェクトのリストを取得するオプション、created_atで昇順
 
     def __str__(self):
-        return self.title
+        return self.title #adminでの表示名
